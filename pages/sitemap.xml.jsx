aad0a578c909606a55a4e110ff82db74b6ad64ce
Crisbern T. Gozun <crisbern@fullstack.ph>
2022-11-23 12:31:54 +0800

    returned GSP

--- pages/sitemap.xml.jsx
+++ pages/sitemap.xml.jsx
@@ -28,7 +28,7 @@ function SiteMap() {
   // getServerSideProps will do the heavy lifting
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps({ res }) {
   // We make an API call to gather the URLs for our site
   const request = await fetch(EXTERNAL_DATA_URL);
   const posts = await request.json();
@@ -36,7 +36,10 @@ export async function getStaticProps() {
   // We generate the XML sitemap with the posts data
   const sitemap = generateSiteMap(posts);
 
+  res.setHeader("Content-Type", "text/xml");
   // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
 
   return {
     props: {},
